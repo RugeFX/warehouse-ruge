928d99e26ff258555aebfba84eac6dd741357913
rugefx <zackfxg@gmail.com>
2023-11-13 23:52:19 +0700

    added react router

--- backend/src/Auth/auth.controller.js
+++ backend/src/Auth/auth.controller.js
@@ -1,59 +1,55 @@
-const express = require("express")
-const multer = require('multer')
+const express = require("express");
+const multer = require("multer");
 const { loginSchema } = require("../Validation/validation");
 const { getUser } = require("./auth.service");
-const jwt = require("jsonwebtoken");  
+const jwt = require("jsonwebtoken");
 const prisma = require("../db");
 
 const router = express.Router();
 
-router.post('/login',multer().none(),async (req,res)=>{
-    try{
-        const {username,password} = req.body
-        await loginSchema.validateAsync({username:username,password:password})
-        const user = await getUser(username,password)
-        const payload = {
-            id:user.id,
-            username:user.username,
-            password:user.password,
-            staffId:user.staffId,
-        }
-        const token = jwt.sign(payload,process.env.SECRET_SERVER,{expiresIn:"15s"})
-        const refreshtoken = jwt.sign(payload,process.env.SECRET_SERVER,{expiresIn:"7d"})
+router.post("/login", multer().none(), async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    await loginSchema.validateAsync({ username: username, password: password });
+    const user = await getUser(username, password);
+    const payload = {
+      id: user.id,
+      username: user.username,
+      password: user.password,
+      staffId: user.staffId,
+    };
+    const token = jwt.sign(payload, process.env.SECRET_SERVER, { expiresIn: "15s" });
+    const refreshtoken = jwt.sign(payload, process.env.SECRET_SERVER, { expiresIn: "7d" });
 
-        const expireAt = new Date(Date.now()+7)
-        expireAt.setDate(expireAt.getDate()+7)
-        
-        await prisma.refreshToken.create({
-            data:{
-                token:refreshtoken,
-                expireAt:expireAt
-            }
-        })
-        res.json({
-            message: 'Login successful',
-            user: user,
-            token:token,
-            refreshtoken:refreshtoken
-          });
+    const expireAt = new Date(Date.now() + 7);
+    expireAt.setDate(expireAt.getDate() + 7);
+
+    await prisma.refreshToken.create({
+      data: {
+        token: refreshtoken,
+        expireAt: expireAt,
+      },
+    });
+    res.json({
+      message: "Login successful",
+      user: user,
+      token: token,
+      refreshtoken: refreshtoken,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({
+      error: error.message || "Unexpected error",
+    });
+  }
+});
+
+router.post("/token", async (req, res) => {
+  try {
+    const { refreshToken } = req.body;
+    if (!refreshToken) {
     }
-    catch(error){
-        res.status(400).json({
-            error: error.message || 'Unexpected error',
-          });
-    }
-
-})
-
-router.post("/token",async(req,res)=>{
-    try{
-        const {refreshToken} = req.body
-        if(!refreshToken){
-            
-        }
-    }catch(err){
-
-    }
-})
+  } catch (err) {}
+});
 
 module.exports = router;

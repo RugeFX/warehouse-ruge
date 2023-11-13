68b6325a0709a9cd074ca4c4065102321fe4a6c8
rugefx <zackfxg@gmail.com>
2023-11-14 05:28:47 +0700

    make base

--- frontend/src/api/apiClient.ts
+++ frontend/src/api/apiClient.ts
@@ -1,14 +1,10 @@
 import axios from "axios";
-import type { RefreshResponse } from "@/types/response";
 
-export const apiClient = axios.create({
+const apiClient = axios.create({
   baseURL: import.meta.env.VITE_BASE_API_URL ?? "http://localhost:5000",
   withCredentials: true,
 });
 
 apiClient.defaults.headers.common["Content-Type"] = "application/json";
 
-export const refreshAccessToken = async () => {
-  const response = await apiClient.post<RefreshResponse>("auth/token");
-  return response.data;
-};
+export default apiClient;

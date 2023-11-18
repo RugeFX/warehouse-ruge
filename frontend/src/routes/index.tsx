4639c17294d8ce1acc9a92da20071e9f81632cbb
rugefx <zackfxg@gmail.com>
2023-11-19 02:09:33 +0700

    added router and route protection components

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,24 +1,50 @@
 import HomePage from "@/pages/HomePage";
+import LoginPage from "@/pages/LoginPage";
+import NotFoundPage from "@/pages/NotFoundPage";
 import TodosPage from "@/pages/TodosPage";
 import DashboardPage from "@/pages/dashboard/DashboardPage";
-import { createBrowserRouter } from "react-router-dom";
+import { type RouteObject, createBrowserRouter } from "react-router-dom";
+import PrivateRoute from "./PrivateRoute";
+import GuestRoute from "./GuestRoute";
+import AccessTestPage from "@/pages/AccessTestPage";
+
+const privateRoutes: RouteObject = {
+  element: <PrivateRoute />,
+  children: [
+    {
+      path: "dashboard",
+      element: <DashboardPage />,
+    },
+    {
+      path: "todos",
+      element: <TodosPage />,
+    },
+    {
+      path: "access",
+      element: <AccessTestPage />,
+    },
+  ],
+};
+
+const guestRoutes: RouteObject = {
+  element: <GuestRoute />,
+  children: [
+    {
+      index: true,
+      element: <HomePage />,
+    },
+    {
+      path: "login",
+      element: <LoginPage />,
+    },
+  ],
+};
 
 export const router = createBrowserRouter([
+  guestRoutes,
+  privateRoutes,
   {
-    path: "/",
-    children: [
-      {
-        index: true,
-        element: <HomePage />,
-      },
-      {
-        path: "todos",
-        element: <TodosPage />,
-      },
-      {
-        path: "dashboard",
-        element: <DashboardPage />,
-      },
-    ],
+    path: "*",
+    element: <NotFoundPage />,
   },
 ]);
